a7d038ac77b1046657421ac16eeab11ce534644a
remco van der Velde <remco.pc@outlook.com>
2023-12-30 13:48:54 +0100

    init

--- src/Package/Trait/Configure.php
+++ src/Package/Trait/Configure.php
@@ -30,101 +30,12 @@ trait Configure {
         if($object->config(Config::POSIX_ID) !== 0){
             return;
         }
-        ddd($options);
-
-
-        //php and apache2 should be installed by docker.
-        //if there is a different sury package, there are multiple versions
-        $dir = new Dir();
-        $read = $dir->read('/etc/php/');
-        $read = Sort::list($read)->with(['name' => 'desc']);
-        $file_old = false;
-        if(count($read) === 1){
-            $file = current($read);
-        } else {
-            $file = current($read);
-            $file_old = end($read);
-        }
-        $fpm = 'php' . $file->name . '-fpm';
-        if($file_old){
-            $php = 'php' . $file_old->name;
-        } else {
-            $php = 'php' . $file->name;
-        }
-        Dir::create('/run/php');
-        $command = 'a2enmod proxy_fcgi setenvif';
-        Core::execute($object, $command, $output, $notification);
-        if(!empty($output)){
-            echo $output . PHP_EOL;
-        }
-        if(!empty($notification)){
-            echo $notification . PHP_EOL;
-        }
-        $command = 'a2enconf ' . escapeshellarg($fpm);
-        Core::execute($object, $command, $output, $notification);
-        if(!empty($output)){
-            echo $output . PHP_EOL;
-        }
-        if(!empty($notification)){
-            echo $notification . PHP_EOL;
-        }
-        $command = 'a2dismod ' . escapeshellarg($php);
-        Core::execute($object, $command, $output, $notification);
-        if(!empty($output)){
-            echo $output . PHP_EOL;
-        }
-        if(!empty($notification)){
-            echo $notification . PHP_EOL;
-        }
-        $command = 'a2dismod mpm_prefork';
-        Core::execute($object, $command, $output, $notification);
-        if(!empty($output)){
-            echo $output . PHP_EOL;
-        }
-        if(!empty($notification)){
-            echo $notification . PHP_EOL;
-        }
-        $command = 'a2enmod mpm_event';
-        Core::execute($object, $command, $output, $notification);
-        if(!empty($output)){
-            echo $output . PHP_EOL;
-        }
-        if(!empty($notification)){
-            echo $notification . PHP_EOL;
-        }
-        $command = 'a2enmod http2';
-        Core::execute($object, $command, $output, $notification);
-        if(!empty($output)){
-            echo $output . PHP_EOL;
-        }
-        if(!empty($notification)){
-            echo $notification . PHP_EOL;
-        }
-        $command = 'a2enmod rewrite';
-        Core::execute($object, $command, $output, $notification);
-        if(!empty($output)){
-            echo $output . PHP_EOL;
-        }
-        if(!empty($notification)){
-            echo $notification . PHP_EOL;
-        }
-        $command = 'a2enmod ssl';
-        Core::execute($object, $command, $output, $notification);
-        if(!empty($output)){
-            echo $output . PHP_EOL;
-        }
-        if(!empty($notification)){
-            echo $notification . PHP_EOL;
-        }
-        $command = 'a2enmod md';
-        Core::execute($object, $command, $output, $notification);
-        if(!empty($output)){
-            echo $output . PHP_EOL;
-        }
-        if(!empty($notification)){
-            echo $notification . PHP_EOL;
-        }
-        $command = '. /etc/apache2/envvars';
+        $command = Core::binary($object) .
+            ' r3m_io/basic apache2 site create' .
+            ' -server.admin=development@universeorange.com' .
+            ' -server.name=example.com' .
+            ' -development'
+        ;
         Core::execute($object, $command, $output, $notification);
         if(!empty($output)){
             echo $output . PHP_EOL;
